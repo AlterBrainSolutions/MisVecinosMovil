3128b904d3bb8315ec447a3f26d69a3e79813474
JS2202 <60891410+JS2202@users.noreply.github.com>
2021-08-12 22:54:59 -0500

    Compartir QR
    Permite compartir el QR generado por RS, Correo, o cualquier otro medio.

--- app/src/main/java/alterbrain/com/ui/AcpAccesoDialogFragment.java
+++ app/src/main/java/alterbrain/com/ui/AcpAccesoDialogFragment.java
@@ -2,29 +2,40 @@ package alterbrain.com.ui;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import net.glxn.qrgen.android.QRCode;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import alterbrain.com.BuildConfig;
 import alterbrain.com.R;
 import alterbrain.com.app.Constantes;
 
 public class AcpAccesoDialogFragment extends DialogFragment {
 
-
+    Button btnCompartir;
     String nombre, fecha, comentario,tipo, tx, mensaje;
     TextView tvDatos;
-
+    Bitmap bitmap;
     ImageView ivCerrar;
+    ImageView imagenCodigo;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,21 +51,21 @@ public class AcpAccesoDialogFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.acp_acceso_full_dialog, container, false);
 
         ivCerrar = view.findViewById(R.id.imageViewCerrarAcp);
+        btnCompartir = view.findViewById(R.id.btnCompartir);
         nombre = Constantes.NOMBRE_ACCE;
         fecha = Constantes.FECHA_ACCE;
         comentario = Constantes.COMENTARIO_ACCE;
         tipo = Constantes.TIPO_ACCE;
         tx = nombre+"\n"+tipo+"\n"+fecha+"\n"+comentario;
-
         tvDatos = view.findViewById(R.id.textViewDatosAcc);
         tvDatos.setText(tx);
 
         eventos();
 
         String texto = Constantes.COD_ACCE;
-        Bitmap bitmap = QRCode.from(texto).bitmap();
+        bitmap = QRCode.from(texto).bitmap();
         // Suponiendo que tienes un ImageView con el id ivCodigoGenerado
-        ImageView imagenCodigo = view.findViewById(R.id.imageViewQR);
+        imagenCodigo = view.findViewById(R.id.imageViewQR);
         imagenCodigo.setImageBitmap(bitmap);
 
         return view;
@@ -71,6 +82,36 @@ public class AcpAccesoDialogFragment extends DialogFragment {
                     showDialogConfirm();
                 } else {
                     getDialog().dismiss();
+                }
+            }
+        });
+
+        btnCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // save bitmap to cache directory
+                try {
+                    File cachePath = new File(getActivity().getCacheDir(), "images");
+                    cachePath.mkdirs(); // don't forget to make the directory
+                    FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    stream.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                File imagePath = new File(getActivity().getCacheDir(), "images");
+                File newFile = new File(imagePath, "image.png");
+                Uri contentUri = FileProvider.getUriForFile(getActivity(), BuildConfig.APPLICATION_ID + ".fileprovider", newFile);
+
+                if (contentUri != null) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+                    shareIntent.setDataAndType(contentUri, getActivity().getContentResolver().getType(contentUri));
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                    shareIntent.setType("image/png");
+                    startActivity(Intent.createChooser(shareIntent, "Choose an app"));
                 }
             }
         });
